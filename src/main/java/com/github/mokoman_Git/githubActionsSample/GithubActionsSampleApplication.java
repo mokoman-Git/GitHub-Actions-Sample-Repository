d6aac03f7ba5dbd8be168657d887818fcf0eea32
mokoman <takaya5460c4@gmail.com>
2026-06-21 15:58:29 +0900

    git initializetion

---
+++ src/main/java/com/github/mokoman_Git/githubActionsSample/GithubActionsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.mokoman_Git.githubActionsSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mokoman_Git/githubActionsSample/GithubActionsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mokoman_Git.githubActionsSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
